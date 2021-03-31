be8437f19a47f732e2d995d01621564f70e40942
Raju979 <thaparaju979@gmail.com>
2021-03-31 16:52:33 +0545

    added category fragment

--- app/src/main/java/com/rajuthapa/todoapp/ui/category/AddUpdateCatFragment.java
+++ app/src/main/java/com/rajuthapa/todoapp/ui/category/AddUpdateCatFragment.java
@@ -61,6 +61,8 @@ public class AddUpdateCatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_update_cat, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_update_cat, container, false);
+
+        return v;
     }
 }
